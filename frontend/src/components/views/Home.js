2ed36080cb031c54de891e7506afef2a53e772f2
Deshan Rajapaksha <98569506+Desh19@users.noreply.github.com>
2023-04-09 19:47:12 +0530

    setup homepage
    setup homepage

---
+++ frontend/src/components/views/Home.js
@@ -0,0 +1,15 @@
+// import React, { useEffect } from 'react';
+// import axios from 'axios';
+// const Swal = require('sweetalert2')
+
+const Home = () => {
+
+  return (
+    <div>
+        <h1>home page</h1>
+    </div>
+  )
+}
+
+
+export default Home
