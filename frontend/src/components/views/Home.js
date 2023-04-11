8751dc35876f0a9d8517ebaafc9f38d5c156df98
Deshan Rajapaksha <98569506+Desh19@users.noreply.github.com>
2023-04-11 09:49:38 +0530

    home page
    home page

--- frontend/src/components/views/Home.js
+++ frontend/src/components/views/Home.js
@@ -2,12 +2,28 @@
 // import axios from 'axios';
 // const Swal = require('sweetalert2')
 
+import './home.css';
+import BgVideo from '../videos/bgVideo.mp4';
+
 const Home = () => {
 
   return (
     <div>
-        <h1>home page</h1>
-    </div>
+          <div className="landingpage">
+
+          <video src={BgVideo} autoPlay muted loop class="video-bg" />
+          <div className="bg-overlay"></div>
+
+          <div className="home-text">
+              <h1>#VIST SRILANKA</h1>
+              <p>Come live out your ideal vacation with us</p>
+          </div>
+
+          <div className="home-btn">About Us</div>
+
+          </div>
+
+    </div>  
   )
 }
 
