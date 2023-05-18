e99192bb212931c1fd4a635d50b6f49e7d53c88e
Deshan Rajapaksha <98569506+Desh19@users.noreply.github.com>
2023-05-18 22:44:42 +0530

    footer changes
    footer changes

--- frontend/src/components/views/Footer.js
+++ frontend/src/components/views/Footer.js
@@ -25,8 +25,39 @@ const Footer = () => {
                 </div>
   
                 <div id="contactus" class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">Contact Us</h5>
-
+                    <h5 class="text-uppercase ms-2">Contact Us</h5>
+    
+                <a
+                  class="btn text-white btn-floating "
+                  style= {{ color: "#3b5998", fontSize:25 }}
+                  href="#!"
+                  role="button"
+                  ><i class="fa-brands fa-facebook" style= {{ color: "#3b5998" }} ></i></a>
+                <a
+                  class="btn text-white btn-floating m-1"
+                  style= {{ color: "#55acee", fontSize:25 }}
+                  href="#!"
+                  role="button"
+                  ><i class="fa-brands fa-twitter" style= {{ color: "#55acee" }}></i></a>
+                <a
+                  class="btn text-white btn-floating m-1"
+                  style= {{ color: "#dd4b39", fontSize:25 }}
+                  href="#!"
+                  role="button"
+                  ><i class="fa-brands fa-google" style= {{ color: "#dd4b39" }}></i></a>
+                <a
+                  class="btn text-white btn-floating m-1"
+                  style= {{ color: "#ac2bac", fontSize:25 }}
+                  href="#!"
+                  role="button"
+                  ><i class="fa-brands fa-instagram" style= {{ color: "#ac2bac" }}></i></a>
+                <a
+                  class="btn text-white btn-floating m-1"
+                  style= {{ color: "#0082ca", fontSize:25 }}
+                  href="#!"
+                  role="button"
+                  ><i class="fa-brands fa-linkedin" style= {{ color: "#0082ca" }}></i></a>
+              
                 </div>
             
                 </div>
